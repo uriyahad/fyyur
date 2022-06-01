2dad706a73461b2429e7392913f64d019d727ffd
Uriyah Sam <uriyah@uriyah>
2022-06-01 01:25:05 -0600

    second commit

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = '<postgresql://uriyah:avestars@localhost:5432/todoapp>'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
